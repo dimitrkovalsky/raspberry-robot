06995d39cc5e585ed037e617331b82aaa4374d42
dkovalskyi <dkovalskyi@edmunds.com>
2015-11-21 12:22:23 +0200

    Improved

--- src/main/java/com/liberty/robot/beans/GPIOBean.java
+++ src/main/java/com/liberty/robot/beans/GPIOBean.java
@@ -12,26 +12,36 @@ import com.pi4j.io.gpio.RaspiPin;
 public class GPIOBean {
     private boolean turnedOn = false;
     private final GpioController gpio = GpioFactory.getInstance();
-    private  GpioPinDigitalOutput pin7 = null;
-    private  GpioPinDigitalOutput pin25 = null;
+    private  GpioPinDigitalOutput redPin = null;
+    private  GpioPinDigitalOutput greenPin = null;
+    private  GpioPinDigitalOutput yellowPin = null;
     public void init(){
         System.out.println("Initializing GPIOBean");
-        pin7 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_07, "MyLED", PinState.LOW);
-        pin25 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_25, "MyLED25", PinState.LOW);
+        redPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_19, "MyLED11", PinState.LOW);
+        greenPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_21, "MyLED12", PinState.LOW);
+        yellowPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_11, "MyLED13", PinState.LOW);
 
-        pin7.setShutdownOptions(true, PinState.LOW);
-        pin25.setShutdownOptions(true, PinState.LOW);
+        redPin.setShutdownOptions(true, PinState.LOW);
+        greenPin.setShutdownOptions(true, PinState.LOW);
+        yellowPin.setShutdownOptions(true, PinState.LOW);
     }
 
     public  void toggle(){
         try {
-
             System.out.println("[GPIOBean] toggle : " + !turnedOn);
             turnedOn = !turnedOn;
-
-            pin7.toggle();
-            pin25.toggle();
-
+            yellowPin.setState(turnedOn);//high();
+            if(turnedOn){
+                greenPin.high();
+                redPin.low();
+            } else {
+                greenPin.low();
+                redPin.high();
+            }
+            System.out.println("YELLOW address" + yellowPin.getPin().getAddress());
+            System.out.println("Y : " + yellowPin.getState());
+            System.out.println("R : " + redPin.getState());
+            System.out.println("G : " + greenPin.getState());
         }   catch(Exception e){
             System.err.println("[GPIOBean] error : "  + e.getMessage());
         }
